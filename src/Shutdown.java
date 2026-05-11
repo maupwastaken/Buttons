d0bd27b204552be3bf4186556d9ca32d95051d00
maupwastaken <jan.schlegel.personal@proton.me>
2026-05-11 08:22:28 +0200

    added constructor

--- src/Shutdown.java
+++ src/Shutdown.java
@@ -1,6 +1,10 @@
 import basis.Knopf;
 
 public class Shutdown extends Knopf {
+    public Shutdown(String name, int x1, int x2, int y1, int y2) {
+        super(name, x1, x2, y1, y2);
+    }
+
     public void execute() {
         try {
             Process process = new ProcessBuilder("systemctl", "poweroff")
